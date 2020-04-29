16334e9005ab54812086ae32b7eb68a78e8d53c7
Sebastian Di Luzio <sebidiluzio@gmail.com>
2020-04-29 18:48:11 +0200

    linting, which was for some reason lost?

--- server/app.ts
+++ server/app.ts
@@ -1,40 +1,40 @@
 /* eslint-disable import/first */ // we do this so we can call dotenv first
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
-if (process.env.NODE_ENV === "development" && !process.env.DATABASE_URL) {
+if (process.env.NODE_ENV === 'development' && !process.env.DATABASE_URL) {
   throw new Error(`[FATAL ERROR] Missing environment variables. 
 This Error is thrown if the database connection URL is not defined, which let's us assume that the entire .env is not available.`);
 }
 
-import express from "express";
-import cookieParser from "cookie-parser";
-import logger from "morgan";
-import cors from "cors";
-import consola from "consola";
-import { Nuxt, Builder } from "nuxt";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors';
+import consola from 'consola';
+import { Nuxt, Builder } from 'nuxt';
 // import { matchUsersInBackground, updateUsersInBackground } from './backgroundMatcher';
-import Umzug from "umzug";
-import { Sequelize } from "sequelize";
-import routeSetup from "./routes";
-import { chatSocketSetup } from "./chatSockets";
-import config from "../nuxt.config";
-import { sequelize } from "./IDP_code/init_db";
-import { run } from "./switch-share/twitter";
+import Umzug from 'umzug';
+import { Sequelize } from 'sequelize';
+import routeSetup from './routes';
+import { chatSocketSetup } from './chatSockets';
+import config from '../nuxt.config';
+import { sequelize } from './IDP_code/init_db';
+import { run } from './switch-share/twitter';
 
 const umzug = new Umzug({
-  storage: "sequelize",
+  storage: 'sequelize',
   storageOptions: {
-    sequelize // here should be a sequelize instance, not the Sequelize module
+    sequelize, // here should be a sequelize instance, not the Sequelize module
   },
   migrations: {
     params: [
       sequelize.getQueryInterface(),
-      Sequelize // Sequelize constructor - the required module
+      Sequelize, // Sequelize constructor - the required module
     ],
-    path: "./migrations",
-    pattern: /\.js$/
-  }
+    path: './migrations',
+    pattern: /\.js$/,
+  },
 });
 
 function loopWithStart(fun: Function, delay: number, runFirst = false) {
@@ -50,13 +50,13 @@ const app = express();
 app.use(cors());
 // test, use should do this already but who knows
 // allow OPTIONS on all resources
-app.options("*", cors());
-app.use(logger("dev"));
+app.options('*', cors());
+app.use(logger('dev'));
 app.use(express.json());
 app.use(
   express.urlencoded({
-    extended: false
-  })
+    extended: false,
+  }),
 );
 app.use(cookieParser());
 
@@ -67,40 +67,40 @@ routeSetup(app);
 chatSocketSetup(app);
 
 // Import and Set Nuxt.js options
-config.dev = !(process.env.NODE_ENV === "production");
+config.dev = !(process.env.NODE_ENV === 'production');
 
 async function start() {
   try {
-    console.info("[UMZUG] Applying pending migrations.");
+    console.info('[UMZUG] Applying pending migrations.');
     const migrations = await umzug.up();
     if (migrations.length > 0) {
-      console.info("[UMZUG] Applied migrations:");
+      console.info('[UMZUG] Applied migrations:');
       /* eslint-disable no-restricted-syntax */
       for (const m of migrations) {
         console.info(` -  ${m.file}`);
       }
       /* eslint-enable no-restricted-syntax */
     } else {
-      console.info("[UMZUG] Database is up to date.");
+      console.info('[UMZUG] Database is up to date.');
     }
   } catch (e) {
-    console.error("[UMZUG] Failed migrating database:");
+    console.error('[UMZUG] Failed migrating database:');
     console.error(e);
     process.exit();
   }
 
   const syncSequelizeModels = true;
-  if (syncSequelizeModels && process.env.NODE_ENV === "development") {
+  if (syncSequelizeModels && process.env.NODE_ENV === 'development') {
     console.info(
-      "[SEQUELIZE] Starting to sync defined tables to DB because we are in dev mode."
+      '[SEQUELIZE] Starting to sync defined tables to DB because we are in dev mode.',
     );
     const wipeDB = false; // set to true if you changed the DB and are in dev mode
     // sync force apparently also wipes the SequelizeMeta table,
     // which then errors on re-trying migrations
     await sequelize.sync({
-      force: wipeDB && process.env.NODE_ENV === "development"
+      force: wipeDB && process.env.NODE_ENV === 'development',
     });
-    console.info("[SEQUELIZE] Finished syncing defined tables to DB.");
+    console.info('[SEQUELIZE] Finished syncing defined tables to DB.');
   }
 
   // Init Nuxt.js
@@ -108,8 +108,8 @@ async function start() {
   await nuxt.ready();
 
   const {
-    host = process.env.HOST || process.env.NUXT_HOST || "127.0.0.1",
-    port = process.env.PORT || process.env.NUXT_PORT || 3000
+    host = process.env.HOST || process.env.NUXT_HOST || '127.0.0.1',
+    port = process.env.PORT || process.env.NUXT_PORT || 3000,
   } = nuxt.options.server;
 
   // Build only in dev mode
@@ -118,7 +118,7 @@ async function start() {
     await builder.build();
   }
   // Handle production
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.NODE_ENV === 'production') {
     // Static folder
     app.use(express.static(`${__dirname}/../dist/`));
   }
@@ -128,7 +128,7 @@ async function start() {
 
   consola.ready({
     message: `Server listening on http://${host}:${port}`,
-    badge: true
+    badge: true,
   });
 
   // start switch share code
